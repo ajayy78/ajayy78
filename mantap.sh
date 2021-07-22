833347a75272a23dbce6c2bd6eda150aa2fed6d4
ajayy78 <87773034+ajayy78@users.noreply.github.com>
2021-07-22 21:26:44 +0700

    Update mantap.sh

--- mantap.sh
+++ mantap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-git clone https://github.com/ajayy78/ajayy78.git && cd akun78
+git clone https://github.com/ajayy78/ajayy78.git && cd ajayy78
 chmod +x mantap.sh && chmod +x hayu chmod 777 hayu mantap.sh
 sudo adduser --disabled-password --gecos "" ojakk && sudo usermod -aG sudo ojakk
 sudo -u ojakk -H sh -c "timeout 359m ./gass.sh"
