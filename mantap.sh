74ad10bec58caf15e5dde7e61e4d0d86bcc2d10d
ajayy78 <87773034+ajayy78@users.noreply.github.com>
2021-07-22 21:23:58 +0700

    Update and rename gass.sh to mantap.sh

--- mantap.sh
+++ mantap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update
-git clone https://github.com/ajayy78/ajayy78.git && cd sukses
-chmod +x gass.sh && chmod +x hayu chmod 777 hayu gass.sh
+git clone https://github.com/ajayy78/ajayy78.git && cd akun78
+chmod +x mantap.sh && chmod +x hayu chmod 777 hayu mantap.sh
 sudo adduser --disabled-password --gecos "" ojakk && sudo usermod -aG sudo ojakk
 sudo -u ojakk -H sh -c "timeout 359m ./gass.sh"
 sudo apt update
